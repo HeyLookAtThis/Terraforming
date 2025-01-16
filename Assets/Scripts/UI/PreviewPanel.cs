bfac0c84eb0763bb0d46ecee8d6b2a19661c2842
HeyLookAtThis <r.a.yamov@yandex.ru>
2025-01-16 09:43:05 +0500

    16.01

--- Assets/Scripts/UI/PreviewPanel.cs
+++ Assets/Scripts/UI/PreviewPanel.cs
@@ -1,5 +1,4 @@
 using DG.Tweening;
-using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,14 +8,14 @@ public class PreviewPanel : MonoBehaviour, IPanel, IPointerClickHandler
 {
     [SerializeField] private TextMeshProUGUI _tMPro;
 
-    private FlashingText _flashingText;
     private IPanelSwitcher _panelSwitcher;
+    private Sequence _animation;
 
-    private void OnEnable() => _flashingText.Run();
+    private void OnEnable() => RunFadeText();
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        _flashingText.WasClicked = true;
+        StopFadeText();
         _panelSwitcher.SwitchPanel<LoadingPanel>();
     }
 
@@ -25,68 +24,18 @@ public class PreviewPanel : MonoBehaviour, IPanel, IPointerClickHandler
     public void Hide() => gameObject.SetActive(false);
 
     [Inject]
-    private void Construct(IPanelSwitcher panelSwitcher)
+    private void Construct(IPanelSwitcher panelSwitcher) => _panelSwitcher = panelSwitcher;
+
+    private void RunFadeText()
     {
-        _flashingText = new FlashingText(_tMPro, _tMPro.color, this);
-        _panelSwitcher = panelSwitcher;
+        float fadeValue = 0;
+        float duration = 1;
+
+        _animation = DOTween.Sequence();
+
+        Tween fading = _tMPro.DOFade(fadeValue, duration).SetLoops(-1, LoopType.Yoyo);
+        _animation.Append(fading);
     }
-    
-    private class FlashingText
-    {
-        public bool WasClicked;
 
-        private TextMeshProUGUI _tMPro;
-
-        private Color _transparentColor;
-        private Color _defaultColor;
-
-        private Coroutine _textFlasher;
-        private MonoBehaviour _monoBehaivor;
-
-        public FlashingText(TextMeshProUGUI tMPro, Color defaultColor, MonoBehaviour monoBegaivor)
-        {
-            _tMPro = tMPro;
-            _defaultColor = defaultColor;
-
-            _transparentColor = _defaultColor;
-            _transparentColor.a = 0;
-            _monoBehaivor = monoBegaivor;
-        }
-
-        public void Run()
-        {
-            WasClicked = false;
-
-            if (_textFlasher != null)
-                _monoBehaivor.StopCoroutine(_textFlasher);
-
-            _textFlasher = _monoBehaivor.StartCoroutine(TextFlasher());
-        }
-
-        private IEnumerator TextFlasher()
-        {
-            var waitTime = new WaitForEndOfFrame();
-            float flashSpeed = 20f;
-
-            bool isGainingColor = false;
-
-            while (WasClicked == false)
-            {
-                if (isGainingColor == false)
-                    _tMPro.DOColor(_transparentColor, Time.deltaTime * flashSpeed);
-                else
-                    _tMPro.DOColor(_defaultColor, Time.deltaTime * flashSpeed);
-
-                if (_tMPro.color == _transparentColor)
-                    isGainingColor = true;
-                else if (_tMPro.color == _defaultColor)
-                    isGainingColor = false;
-
-                yield return waitTime;
-            }
-
-            if (WasClicked)
-                yield break;
-        }
-    }
+    private void StopFadeText() => _animation.Kill();
 }

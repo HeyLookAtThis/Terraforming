fa60d2c9c6f86cdce4c632cce2869b880879f9c9
HeyLookAtThis <r.a.yamov@yandex.ru>
2025-02-07 09:03:43 +0500

    07,02

--- Assets/Scripts/InteractiveObjects/LootView.cs
+++ Assets/Scripts/InteractiveObjects/LootView.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -8,6 +9,7 @@ public class LootView : MonoBehaviour
     [SerializeField] private ParticleSystem _particles;
 
     private AudioSource _audioSourse;
+    private Sequence _animation;
 
     public bool IsAllowed => _model.activeSelf;
 
@@ -25,7 +27,7 @@ public class LootView : MonoBehaviour
     {
         if (_model.activeSelf == false)
         {
-            _model.SetActive(true);
+            RunAppeareance();
             _particles.Play();
         }
     }
@@ -33,8 +35,51 @@ public class LootView : MonoBehaviour
     public void TurnOffVisible()
     {
         _particles.Stop();
-        _model.SetActive(false);
+        RunDisapearance();
     }
 
     public void PlaySound() => _audioSourse.Play();
+
+    private void RunAppeareance()
+    {
+        _model.SetActive(true);
+        ResetAnimation();
+
+        float duration = 0.5f;
+
+        Tween appearance = transform.DOScale(Vector3.one, duration).From(Vector3.zero).SetEase(Ease.InBounce);
+        Tween rotation = transform.DORotate(new(0, 360, 0), duration, RotateMode.FastBeyond360);
+
+        _animation.Append(appearance).Append(rotation).OnComplete(() => RunRotateAnimation());
+    }
+
+    private void RunDisapearance()
+    {
+        ResetAnimation();
+
+        float duration = 0.5f;
+
+        Tween appearance = transform.DOScale(Vector3.zero, duration);
+
+        _animation.Append(appearance).OnComplete(() => _model.SetActive(false));
+    }
+
+    private void RunRotateAnimation()
+    {
+        ResetAnimation();
+
+        float duration = 0.5f;
+        float sizeMultiplier = 0.9f;
+
+        Tween resize = transform.DOScale(sizeMultiplier, duration);
+        Tween rotation = transform.DORotate(new(0, 90, 0), duration).SetEase(Ease.OutQuad);
+
+        _animation.Append(resize).Join(rotation).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void ResetAnimation()
+    {
+        _animation?.Kill();
+        _animation = DOTween.Sequence();
+    }
 }
